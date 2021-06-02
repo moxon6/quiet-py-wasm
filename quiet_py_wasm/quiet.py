f75353a75af7b2ebc50553a9c53fe030c5e53649
Martin Moxon <moxon6@googlemail.com>
2021-06-02 10:53:17 +0100

    Extract build_instance from Quie

--- quiet_py_wasm/quiet.py
+++ quiet_py_wasm/quiet.py
@@ -13,40 +13,41 @@ quiet_path = os.path.join(quiet_dir, "quiet.wasm")
 profiles_path = os.path.join(quiet_dir, "quiet-profiles.json")
 
 
+def build_instance():
+    import_object = ImportObject()
+
+    store = Store(engine.JIT(Compiler))
+
+    import_object.register(
+        "env",
+        {
+            "__sys_getpid": Function(store, lambda: 42, FunctionType([], [Type.I32])),
+        }
+    )
+
+    import_object.register(
+        "wasi_snapshot_preview1",
+        {
+            "proc_exit": Function(store, lambda *args: None, FunctionType([Type.I32], [])),
+            "clock_time_get": Function(store, lambda *args: int(time.time()), FunctionType([Type.I32, Type.I64, Type.I32], [Type.I32])),
+            "fd_close": Function(store, lambda *args: 1, FunctionType([Type.I32], [Type.I32])),
+            "fd_write": Function(store, lambda *args: 1, FunctionType([Type.I32, Type.I32, Type.I32, Type.I32], [Type.I32])),
+            "fd_seek": Function(store, lambda *args: 1, FunctionType([Type.I32, Type.I64, Type.I32, Type.I32], [Type.I32])),
+            "fd_read": Function(store, lambda *args: 1, FunctionType([Type.I32, Type.I32, Type.I32, Type.I32], [Type.I32])),
+        }
+    )
+
+    # Let's compile the module to be able to execute it!
+    module = Module(store, open(quiet_path, 'rb').read())
+
+    # Now the module is compiled, we can instantiate it.
+    return Instance(module, import_object)
+
+
 class Quiet:
     def __init__(self):
         self.profiles = json.load(open(profiles_path, 'r'))
-        self.instance = self._build_instance()
-
-    def _build_instance(self):
-        import_object = ImportObject()
-
-        store = Store(engine.JIT(Compiler))
-
-        import_object.register(
-            "env",
-            {
-                "__sys_getpid": Function(store, lambda: 42, FunctionType([], [Type.I32])),
-            }
-        )
-
-        import_object.register(
-            "wasi_snapshot_preview1",
-            {
-                "proc_exit": Function(store, lambda *args: None, FunctionType([Type.I32], [])),
-                "clock_time_get": Function(store, lambda *args: int(time.time()), FunctionType([Type.I32, Type.I64, Type.I32], [Type.I32])),
-                "fd_close": Function(store, lambda *args: 1, FunctionType([Type.I32], [Type.I32])),
-                "fd_write": Function(store, lambda *args: 1, FunctionType([Type.I32, Type.I32, Type.I32, Type.I32], [Type.I32])),
-                "fd_seek": Function(store, lambda *args: 1, FunctionType([Type.I32, Type.I64, Type.I32, Type.I32], [Type.I32])),
-                "fd_read": Function(store, lambda *args: 1, FunctionType([Type.I32, Type.I32, Type.I32, Type.I32], [Type.I32])),
-            }
-        )
-
-        # Let's compile the module to be able to execute it!
-        module = Module(store, open(quiet_path, 'rb').read())
-
-        # Now the module is compiled, we can instantiate it.
-        return Instance(module, import_object)
+        self.instance = build_instance()
 
     def transmit(self, payload, profile):
         Transmitter(self.instance) \
@@ -55,6 +56,6 @@ class Quiet:
             .destroy()
 
     def receive(self, profile):
-        Receiver(self.instance) \
+        return Receiver(self.instance) \
             .select_profile(self.profiles[profile]) \
             .receive()
